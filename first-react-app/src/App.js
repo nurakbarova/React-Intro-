0eaecde20a0a9e68f34097695e0834d607449a40
nurakbarova <nurayakbarova76@gmail.com>
2023-05-02 00:03:59 +0400

    react intro

---
+++ first-react-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './Navbar';
+
+
+function App() {
+
+  return (
+  <React.Fragment>
+ <>
+ <Navbar/>
+ <div>About</div>
+ <footer/>
+ </>
+  </React.Fragment>
+  );
+}
+
+export default App;
